870cd82c5d9c1f5ad30b747e03bfceed3eee634e
Vegard M <94170927+CodeVegard@users.noreply.github.com>
2022-06-24 10:15:12 +0200

    Update script.js

--- javaScript/script.js
+++ javaScript/script.js
@@ -77,39 +77,4 @@ cakeSize.addEventListener('change', function(){
     console.log('Cake size changed');
 })
 
-//--------Practicing "Record collection" functions------------
-// Setup
-const recordCollection = {
-    2548: {
-      albumTitle: 'Slippery When Wet',
-      artist: 'Bon Jovi',
-      tracks: ['Let It Rock', 'You Give Love a Bad Name']
-    },
-    2468: {
-      albumTitle: '1999',
-      artist: 'Prince',
-      tracks: ['1999', 'Little Red Corvette']
-    },
-    1245: {
-      artist: 'Robert Palmer',
-      tracks: []
-    },
-    5439: {
-      albumTitle: 'ABBA Gold'
-    }
-  };
-  
-  // Only change code below this line
-  function updateRecords(records, id, prop, value) {
-        if (prop !== recordCollection[id].tracks && value !== ""){
-            recordCollection.prop = value;
-            console.log(recordCollection);
-        } else (prop === tracks && value !== ""){
-            let emptyArray = [];
-            emptyArray = value;
-            console.log(emptyArray);
-        }
-    return records;
-  }
-  
-  updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+//--------Practicing functions------------
