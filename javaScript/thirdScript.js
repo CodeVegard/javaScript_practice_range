bca6ebe8dc6486ef8b73e0a83325751ac4929d8b
Vegard M <94170927+CodeVegard@users.noreply.github.com>
2022-08-31 12:12:57 +0200

    Update thirdScript.js

--- javaScript/thirdScript.js
+++ javaScript/thirdScript.js
@@ -15,3 +15,27 @@ const pet = {
 import { myVar } from "./fourth.js"
 // the  syntax is ./ because it's the same folder as my current file.
 console.log(myVar);
+
+
+// localStorage practice:
+
+const newVar = "New variable, localStorage testing";
+
+window.localStorage.setItem("variable in local storage", newVar);
+localStorage.setItem("new variable in local storage", "asdasd");
+
+const anotherVariable = localStorage.getItem("new variable in local storage");
+console.log(anotherVariable);
+
+
+const person = {
+  name: "Obaseki Nosa",
+  location: "Lagos",
+}
+window.localStorage.setItem('user', JSON.stringify(person));
+
+let locallyStoredPerson = localStorage.getItem("user");
+console.log(typeof locallyStoredPerson); // string
+
+let parsedPerson = JSON.parse(locallyStoredPerson);
+console.log(typeof parsedPerson); // object
