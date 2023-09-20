c1bc0c91dc50bf2bf27db847699348737d128b24
Vegard M <vm932648@gmail.com>
2023-09-20 13:21:53 +0200

    testing JSDocs
    testing JSDocs

---
+++ javaScript/jsDocsTest.js
@@ -0,0 +1,10 @@
+/**
+ * Adds two numbers together
+ * @param {number} num1 - first number 
+ * @param {number} num2 - second number
+ * @returns 
+ */
+function addNum(num1, num2) {
+    return num1 + num2;
+}
+
