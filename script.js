bb12d73c33e06394ccafd1b82d2dfac81c4a0844
Vegard M <vm932648@gmail.com>
2023-09-22 10:46:45 +0200

    Getting JSDocs to work
    Made JSDocs work, but only with a file inside my root folder, which isn't very productive. Most of my JS is inside a /js/exports folder. Will experiment more

---
+++ script.js
@@ -0,0 +1,15 @@
+/**
+ * Adds two numbers together
+ * @param {number} num1 - first number 
+ * @param {number} num2 - second number
+ * @returns sum of num1 and num2
+ *  
+ * @example
+ * // Enter two numbers, which are added together.
+ * addNum(1,2);
+ * // Returns 3
+ */
+function addNum(num1, num2) {
+    return num1 + num2;
+}
+
