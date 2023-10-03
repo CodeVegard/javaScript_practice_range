c08a0873964aa657d3d3de1e67ccbebdd668f4f1
Vegard M <vm932648@gmail.com>
2023-10-03 12:07:02 +0200

    Playing around with classes
    Figuring out how to work with classes

--- script.js
+++ script.js
@@ -13,3 +13,67 @@ function addNum(num1, num2) {
     return num1 + num2;
 }
 
+//Testing classes below:
+
+class ShoppingCart{
+    cart = [];
+
+    constructor(shopName, currency){
+        this.shopName = shopName;
+        this.currency = currency;
+    }
+
+    addToCart(item){
+        this.cart.push(item);
+    }
+
+    removeFromCart(item){
+        const idToFind = item.id;
+        const indexToRemove = this.cart.findIndex(
+            (currentItem) => currentItem.id === idToFind,
+        );
+        if(indexToRemove === -1){
+            return null;
+        }
+
+        const newCart = this.cart.filter((item, index) => index !== indexToRemove);
+        this.cart = [...newCart]; 
+    }
+
+    calculateTotalCost(){
+        const totalCost = this.cart.reduce((total, item)  => {
+            total += item.price;
+            return total;
+        }, 0);
+        return totalCost;
+    }
+
+    displayCart(){
+        console.log("Your Cart:")
+        console.log("----------------------------");
+        this.cart.forEach((item) => {
+            console.log(item.title);
+        });
+        console.log("============================")
+    }
+
+    displayTotalCost(){
+        console.log("Total items:" + this.cart.length);
+        console.log("Total is " + this.calculateTotalCost() + " " + this.currency);
+    }
+}
+
+const myCart = new ShoppingCart("Bagles and Brass", "NOK");
+
+const cookies = { id: 23, title: 'Chocolate Chip Cookies', price: 20.0 };
+const cake = { id: 45, title: 'Vanilla Cake', price: 30.0 };
+
+myCart.addToCart(cookies);
+myCart.addToCart(cookies);
+myCart.addToCart(cake);
+
+myCart.displayCart();
+
+myCart.removeFromCart(cookies);
+myCart.displayCart();
+myCart.displayTotalCost();
