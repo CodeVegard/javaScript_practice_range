adf3cc55b39f4d019ec7b09a1e55224d5af4b418
Vegard M <vm932648@gmail.com>
2023-10-09 14:01:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,71 @@
-/**
- * Adds two numbers together
- * @param {number} num1 - first number 
- * @param {number} num2 - second number
- * @returns sum of num1 and num2
- *  
- * @example
- * // Enter two numbers, which are added together.
- * addNum(1,2);
- * // Returns 3
- */
-function addNum(num1, num2) {
-    return num1 + num2;
-}
+// /**
+//  * Adds two numbers together
+//  * @param {number} num1 - first number 
+//  * @param {number} num2 - second number
+//  * @returns sum of num1 and num2
+//  *  
+//  * @example
+//  * // Enter two numbers, which are added together.
+//  * addNum(1,2);
+//  * // Returns 3
+//  */
+// function addNum(num1, num2) {
+//     return num1 + num2;
+// }
 
-//Testing classes below:
+// //Testing classes below:
 
-class Person {
-    constructor(firstName, lastName) {
-      this.firstName = firstName;
-      this.lastName  = lastName;
-    }
+// class Person {
+//     constructor(firstName, lastName) {
+//       this.firstName = firstName;
+//       this.lastName  = lastName;
+//     }
 
-    speak(){
-        console.log(`Hello ${this.firstName} ${this.lastName}`)
-    }
+//     speak(){
+//         console.log(`Hello ${this.firstName} ${this.lastName}`)
+//     }
 
-  }
+//   }
 
-const vegard = new Person("Vegard", "Maaø");
-vegard.speak();
+// const vegard = new Person("Vegard", "Maaø");
+// vegard.speak();
 
-  // Testing filters
-const games = [
-  { title: 'Mayhem Fighter', isMultiplayer: true, rating: 8 },
-  { title: 'Build-a-farm', isMultiplayer: true, rating: 9 },
-  { title: 'Ghost Story', isMultiplayer: false, rating: 8 },
-  { title: 'Fast Car Racer', isMultiplayer: true, rating: 7 },
-  { title: 'Elf and Dwarf RPG', isMultiplayer: false, rating: 8 },
+//   // Testing filters
+// const games = [
+//   { title: 'Mayhem Fighter', isMultiplayer: true, rating: 8 },
+//   { title: 'Build-a-farm', isMultiplayer: true, rating: 9 },
+//   { title: 'Ghost Story', isMultiplayer: false, rating: 8 },
+//   { title: 'Fast Car Racer', isMultiplayer: true, rating: 7 },
+// //   { title: 'Elf and Dwarf RPG', isMultiplayer: false, rating: 8 },
+// // ];
+
+// const filteredGames = games.filter((game) => game.isMultiplayer === true && game.rating >= 8);
+// console.log(filteredGames);
+
+// Testing reduce();
+
+const products = [
+  { title: 'Cheese', inStock: false, quantity: 2, price: 15.0 },
+  { title: 'Milk', inStock: true, quantity: 5, price: 8.99 },
+  { title: 'Bread', inStock: true, quantity: 3, price: 22.0 },
+  { title: 'Egg', inStock: true, quantity: 12, price: 5 },
 ];
 
-const filteredGames = games.filter((game) => game.isMultiplayer === true && game.rating >= 8);
-console.log(filteredGames);
+// const sum = products.reduce((total, product) => {
+//   if (product.inStock === true) {
+//     total += product.price * product.quantity;
+//     return total;
+//   }
+// }, 0);
+// console.log(sum);
+
+const sum = products.reduce((total, product) => {
+  if (product.inStock){
+    total += product.price * product.quantity;
+  }
+  return total;
+}, 0);
+console.log(sum);
+
+const refractored = products.reduce((total, product) => product.inStock ? (total += product.price * product.quantity) : total, 0);
+console.log(refractored);
