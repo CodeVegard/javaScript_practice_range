9e64d649bfd980b05965f24f2a8b4388a297b9ee
Vegard M <vm932648@gmail.com>
2023-10-09 12:45:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -27,5 +27,17 @@ class Person {
 
   }
 
-  const vegard = new Person("Vegard", "Maaø");
-  vegard.speak();
+const vegard = new Person("Vegard", "Maaø");
+vegard.speak();
+
+  // Testing filters
+const games = [
+  { title: 'Mayhem Fighter', isMultiplayer: true, rating: 8 },
+  { title: 'Build-a-farm', isMultiplayer: true, rating: 9 },
+  { title: 'Ghost Story', isMultiplayer: false, rating: 8 },
+  { title: 'Fast Car Racer', isMultiplayer: true, rating: 7 },
+  { title: 'Elf and Dwarf RPG', isMultiplayer: false, rating: 8 },
+];
+
+const filteredGames = games.filter((game) => game.isMultiplayer === true && game.rating >= 8);
+console.log(filteredGames);
